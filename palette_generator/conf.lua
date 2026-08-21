a228e5656f781600120599adf87d1af7e47edd13
Gustavo <ing.glaragomez@gmail.com>
2026-08-20 20:07:36 -0700

    Working on palette generator

---
+++ palette_generator/conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+  t.identity = "retr01_palette_generator"
+  t.window.title = "Retr01 palette generator"
+  t.window.width = 640
+  t.window.height = 160
+  t.window.resizable = false
+end
